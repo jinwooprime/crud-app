b402dc0b3abcafc8257d6a30f637d6acada53e06
jinwooprime <frostwingconnect@gmail.com>
2024-04-28 17:15:12 -0400

    adding files

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node API Server Updated")
+});
+
+app.get('/api/products/:id', async (req,res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+
+        if (!product) {
+            return res.status(404).json({message: "Product does not exist!"});
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/api/products', async (req,res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post('/api/product/add', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.put('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!product) {
+            return res.status(404).json({message: "Product does not exist!"});
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.delete('/api/product/:id', async (req,res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({message: "Product does not exist!"});
+        }
+
+        res.status(200).json({message: "The product has been deleted!"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+mongoose.connect("mongodb+srv://<username>:<password>@backenddb.2ansun2.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+.then(() => {
+    console.log("Connected to MONGO!");
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000!")
+    });
+})
+.catch(() => {
+    console.log("Connection failed!");
+});
---
+++ models/product.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Product name is required!"]
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Product = mongoose.model("Product", productSchema)
+
+module.exports = Product
